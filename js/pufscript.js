19db701dd37553120f9a89873d802d2b4af2b4d2
Geligimg <gelaipcd@gmail.com>
2025-07-11 12:11:17 +0200

    pufscript.js

--- js/pufscript.js
+++ js/pufscript.js
@@ -10,21 +10,21 @@ if (formularioContacto) {
 
     let errores = [];
 
-    // Validar Teléfono: solo números y mínimo 7 dígitos
+    // Valido Teléfono: solo números y mínimo 7 dígitos
     if (!/^\d{7,}$/.test(telefono)) {
       errores.push(
         "Introduce un número de teléfono válido (solo números, mínimo 7 dígitos)."
       );
     }
 
-    // Validar Email: expresión regular básica para email
+    // Valido el mail
     if (
       !/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(email)
     ) {
       errores.push("Introduce un correo electrónico válido.");
     }
 
-    // Validar Mensaje: mínimo 5 caracteres
+    // Valido Mensaje: mínimo 5 caracteres
     if (mensaje.length < 5) {
       errores.push("El mensaje debe tener al menos 5 caracteres.");
     }
@@ -34,9 +34,11 @@ if (formularioContacto) {
       alert("Por favor corrige los siguientes errores:\n\n" + errores.join("\n"));
     } else {
       alert("Muchas gracias por tu mensaje, te respondemos pronto 🤍🎀");
-      // El formulario se enviará normalmente después de la alerta
+      // El formulario se enviará q después de la alerta
     }
   });
 }
+
+
 
 
